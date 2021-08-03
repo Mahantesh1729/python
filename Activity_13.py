67a502f085534836baf4f8deda321a46b164839a
Mahantesh Gattina <67777625+Mahantesh1729@users.noreply.github.com>
2021-08-03 12:03:14 +0530

    done commit

---
+++ Activity_13.py
@@ -0,0 +1,24 @@
+def main():
+    print("Enter any number: ", end = "")
+    a = input_number()
+    p = isprime(a)
+    display(a, p)
+def input_number():
+    return int(input())
+
+def isprime(a):
+    if a == 1:
+        return False
+    for i in range(2,a,1):
+        if a%i == 0:
+            return False
+    return True
+
+def display(a, p):
+    if p:
+        print(f"{a} is a prime number")
+    else:
+        print(f"{a} is not a prime number")
+
+main()
+    
