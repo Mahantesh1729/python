b37e45c27c656a29ca0c4c1d027d0ab70b89aa13
Mahantesh Gattina <67777625+Mahantesh1729@users.noreply.github.com>
2021-08-04 16:04:44 +0530

    ...

--- Activity_18.py
+++ Activity_18.py
@@ -2,17 +2,22 @@ import re
 def main():
     print("Enter some strings: ")
     s = string_input()
-    s = re.split(';|=', s)
-    dis = make_dict(s)
+    lis = []
+    k = ""
+    for i in s:
+        if i == '=' or i == ';':
+            lis.append(k)
+            k = ""
+        else:
+            k += i
+    dis = make_dict(lis)
     print("Dictionary created")
     display(dis)
     print("Converted back to string")
     s = revert(dis)
     display(s)
-    
 def string_input():
     return input()
-
 def make_dict(s):
     keys = []
     values = []
